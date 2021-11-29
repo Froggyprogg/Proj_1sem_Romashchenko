6f1f18289b2998078cdc744984a260e8630e8552
Oleg <romasenko764@gmail.com>
2021-11-29 14:40:30 +0300

    обновил PZ_5( исправил ошибки). Добавил PZ6

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -2,4 +2,22 @@
 # есть ли в числе одинаковые цифры.
 
 
-def generator(a):
+from random import randint
+
+
+def generator(m):
+    m = randint(1000, 9999)
+    print(m)
+    a = (m // 1000) % 10
+    b = (m // 100) % 10
+    c = (m // 10) % 10
+    d = m % 10
+    return a, b, c, d
+
+
+vxod = 1
+g_1, g_2, g_3, g_4 = generator(vxod)
+if (g_1 == g_2) or (g_1 == g_3) or (g_1 == g_4) or (g_2 == g_3) or (g_2 == g_4) or (g_4 == g_3):
+    print('есть одинаковые')
+else:
+    print('нет одинаковых')
