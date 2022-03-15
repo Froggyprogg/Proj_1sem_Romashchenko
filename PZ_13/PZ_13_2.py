d69fd002d7e7b256862fda64681ebfcff1c9f175
Oleg <romasenko764@gmail.com>
2022-03-15 15:10:41 +0300

    обновил PZ_13_2

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -1,12 +1,10 @@
 # Составить генератор (yield), который преобразует все буквенные символы в строчные.
 
 
-def izm(text_start):
-    for n in text_start:
-        yield text_start.lower()
-        print(text_start)
+def izm(crs: str):
+    for ch in crs:
+        yield ch.lower()
 
 
-text_start = iter(input("Введите текст"))
-text_end = izm(text_start)
-print(text_end)
+text = input("Введите текст")
+print(''.join(izm(text)))
